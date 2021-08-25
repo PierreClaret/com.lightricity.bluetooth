58a04140cef2632a89d10570a656f53b2ce1a102
PierreClaret <claret.pierre74@gmail.com>
2021-08-25 10:48:07 -0400

    no message

--- bluetooth_library/src/main/java/com/lightricity/station/bluetooth/FoundSensor.kt
+++ bluetooth_library/src/main/java/com/lightricity/station/bluetooth/FoundSensor.kt
@@ -2,19 +2,21 @@ package com.lightricity.station.bluetooth
 
 class FoundSensor {
 
-    var co2: Double? = null
     var id: String? = null
     var url: String? = null
     var frame: Int? = null
     var vendorID: Int? = null
     var sensorID: Int? =null
     var rssi: Int? = null
+    var vendor: Int? = null
+    var sensor: Int? = null
     var uuid: Int? = null
     var temperature: Double? = null
     var humidity: Double? = null
     var pressure: Double? = null
     var light: Double? = null
     var sound: Double? = null
+    var co2: Double? = null
     var accelX: Double? = null
     var accelY: Double? = null
     var accelZ: Double? = null
